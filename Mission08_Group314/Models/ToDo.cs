34db44def947d6d101d7508fb8fddaa4d9ae6b99
haileybronson <haileyp7@byu.edu>
2024-02-26 14:26:45 -0700

    more setup

--- Mission08_Group314/Models/ToDo.cs
+++ Mission08_Group314/Models/ToDo.cs
@@ -1,8 +1,25 @@
-﻿using System.Net.Security;
+﻿using Microsoft.Extensions.Options;
+using System.Diagnostics.Metrics;
+using System.Net.Security;
 
 namespace Mission08_Group314.Models
 {
     public class ToDo
     {
+        //Build an app that allow the users to enter tasks with the following information:
+        //Task(Required)
+        //Due Date
+        //Quadrant(Required)
+        //Category(Dropdown containing options: Home, School, Work, Church)
+        //Completed(True/False)
+        public string Task {  get; set; } //primary key 
+
+        public int DueDate { get; set; }
+
+        public int Quadrant { get; set; }
+
+        public string Category { get; set; }
+
+        public bool Completed { get; set; }
     }
 }
--- Mission08_Group314/Models/ToDoFormContext.cs
+++ Mission08_Group314/Models/ToDoFormContext.cs
@@ -2,11 +2,13 @@
 
 namespace Mission08_Group314.Models
 {
-    public class ToDoFormContext
+    public class ToDoFormContext :DbContext
     {
         public ToDoFormContext(DbContextOptions<ToDoFormContext> options): base(options) 
         { 
         }
+
+        //do I need to change ToDo?
 
         public DbSet<ToDo> ToDo { get; set; }
 
