f346146dfcf86c6d1ee8db61639bd43d63100bc9
haileybronson <haileyp7@byu.edu>
2024-02-27 12:33:21 -0700

    final touches

--- Mission08_Group314/Models/ToDoFormContext.cs
+++ Mission08_Group314/Models/ToDoFormContext.cs
@@ -7,6 +7,8 @@ namespace Mission08_Group314.Models
         public ToDoFormContext(DbContextOptions<ToDoFormContext> options): base(options) 
         { 
         }
+
+        //calls the class in the model, sets it to the table of the DB 
         public DbSet<ToDo> ToDo { get; set; }
 
     }
